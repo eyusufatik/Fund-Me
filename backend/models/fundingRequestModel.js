ec3348c075c1799d2073b9f57b38160971ff0fc0
eyusufatik <esadyusufatik@gmail.com>
2022-07-23 14:21:22 +0300

    change funding request model

--- backend/models/fundingRequestModel.js
+++ backend/models/fundingRequestModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const User = require('./userModel')
 
 const fundingRequestSchema = new mongoose.Schema({
-    user: User,
+    userId: { type: ObjectId, required: true },
     title: { type: String, required: true},
     image: { type: String, default: null },
     text: { type: String, required: true }
